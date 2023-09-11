5244f381a0a5163fb0961154ab76331353ce3fbc
Tano Paul <tanopaul38@gmail.com>
2023-09-11 17:06:37 -0400

    completed python loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 11
+    while counter > 1:
+        counter-=1
+        print(counter)
+        if counter == 1:
+            print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [number * number for number in int_list]
+    return squared_list
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1 , 101):
+        if i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
